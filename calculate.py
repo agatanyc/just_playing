068178e8fb436818ff2a873a9aeab2891ed8c8b8
Agata Filho <agata78@gmail.com>
2014-10-02 10:07:50 -0400

    Add calculate program.

---
+++ calculate.py
@@ -0,0 +1,13 @@
+r = 0
+print(r)
+s = input('> ').strip()
+while s:
+    op, n = s[0], int(s[1:]) 
+    if op == '-':
+        r -= n
+    elif op == '+':
+        r += n
+    else:
+        print("Bad operator:", op)
+    print(r)
+    s = input('> ').strip()
