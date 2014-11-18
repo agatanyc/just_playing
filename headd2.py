ddac91adca15f54542deeacc04586411ab6c2afb
Agata Filho <agata78@gmail.com>
2014-11-18 11:39:13 -0500

    Change number of lines printed to 11.

--- headd2.py
+++ headd2.py
@@ -3,7 +3,7 @@ from sys import argv
 def head(fname):
     with open(fname) as file:
         lines = file.readlines()
-        for line in lines[:10]:
+        for line in lines[:11]:
             print(line, end="")
 
 if __name__ == '__main__':
